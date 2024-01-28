c4bdd2a4fb68e3e46a72592d9e2f498cb5d9ee67
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-01-28 15:49:05 +0530

    Custom React

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -4,11 +4,22 @@ import Youtube from "./Youtube"
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  const username = "AdityaNath";
   return (
     <>
       <h1>Jai Sita Raam</h1>
       <Youtube />
+      <h3>Mera naam hai {username}</h3>
+      <h3>This is Vite React App {" "} {/* Empty string here is for extra space */}  #{2+2}</h3> {/* It works absolutely fine */}
+      {/* in JS Object, it literally rendered as a string containing space */}
+
+      {/* But it should not be conditional or something */}
+      {/* Meaning, it should/already be evaluated to some definite value */}
+
+      {/* <h3>Tumhara naam hai { (if(username === "AdityaNath")) ? " ", : "Chor?"} </h3> */}
+      {/* Here it throws ERROR!!! */}
+
+
     </>
   )
 }
